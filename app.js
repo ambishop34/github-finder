ed4f2f4caf4661e49fe4627a99cf22cb326c37d5
ambishop34 <bishopnathan11@gmail.com>
2020-09-09 11:31:54 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,61 @@
+// // Init Github
+// const github = new Github;
+// // Init UI
+// const ui = new UI;
+// //Search input
+// const searchUser = document.getElementById('searchUser');
+
+// // Search input event listener
+// searchUser.addEventListener('keyup', (e) => {
+//     // Get input Text
+//     const userText = e.target.value;
+
+//     if(userText !== ''){
+//        // Make http call
+//        github.getUser(userText)
+//        .then(data => {
+//            if(data.profile.message === 'Not Found') {
+//                // show alert
+//                ui.showAlert('Unfound User', 'alert alert-danger');
+//            } else {
+//                //show pofile
+//                ui.showProfile(data.profile);
+//            }
+//        })
+//     } else {
+//         // Clear profile
+//         ui.clearProfile();
+//     }
+// });
+
+// Init Github
+const github = new Github;
+// Init UI
+const ui = new UI;
+
+// Search input
+const searchUser = document.getElementById('searchUser');
+
+// Search input event listener
+searchUser.addEventListener('keyup', (e) => {
+  // Get input text
+  const userText = e.target.value;
+
+  if(userText !== ''){
+   // Make http call
+   github.getUser(userText)
+    .then(data => {
+      if(data.profile.message === 'Not Found') {
+        // Show alert
+        ui.showAlert('User not found', 'alert alert-danger');
+      } else {
+        // Show profile
+        ui.showProfile(data.profile);
+        ui.showRepos(data.repos);
+      }
+    })
+  } else {
+    // Clear profile
+    ui.clearProfile();
+  }
+});
